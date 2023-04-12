e260ee2f3513acb45509a8ffe55a0f341ca2d9af
rurueuh <maximilien.vanstaevel@epitech.eu>
2023-04-12 21:38:06 +0200

    add build folder

---
+++ build/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game.dir/src/Cube/Cube.cpp.obj"
+  "CMakeFiles/game.dir/src/Cube/Cube.cpp.obj.d"
+  "CMakeFiles/game.dir/src/Renderer/Renderer.cpp.obj"
+  "CMakeFiles/game.dir/src/Renderer/Renderer.cpp.obj.d"
+  "CMakeFiles/game.dir/src/main.cpp.obj"
+  "CMakeFiles/game.dir/src/main.cpp.obj.d"
+  "game.exe"
+  "game.exe.manifest"
+  "game.pdb"
+  "libgame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/myOpengl.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myOpengl.dir/lib/Camera/Camera.cpp.obj"
+  "CMakeFiles/myOpengl.dir/lib/Camera/Camera.cpp.obj.d"
+  "CMakeFiles/myOpengl.dir/lib/Mesh/Mesh.cpp.obj"
+  "CMakeFiles/myOpengl.dir/lib/Mesh/Mesh.cpp.obj.d"
+  "CMakeFiles/myOpengl.dir/lib/Shader/Shader.cpp.obj"
+  "CMakeFiles/myOpengl.dir/lib/Shader/Shader.cpp.obj.d"
+  "CMakeFiles/myOpengl.dir/lib/Texture/Texture.cpp.obj"
+  "CMakeFiles/myOpengl.dir/lib/Texture/Texture.cpp.obj.d"
+  "CMakeFiles/myOpengl.dir/lib/Window/Window.cpp.obj"
+  "CMakeFiles/myOpengl.dir/lib/Window/Window.cpp.obj.d"
+  "libmyOpengl.a"
+  "libmyOpengl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myOpengl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
