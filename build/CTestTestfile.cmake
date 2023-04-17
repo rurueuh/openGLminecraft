d91aad25c4a49a57d685f46a0d8ada5952dd1135
Maximilien Vanstaevel <maximilien.vanstaevel@epitech.eu>
2023-04-17 10:50:56 +0200

    remove build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ruru/Desktop/epitech/perso/openGLminecraft
-# Build directory: /home/ruru/Desktop/epitech/perso/openGLminecraft/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
